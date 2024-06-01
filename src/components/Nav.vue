c7b304bd762463e32ba3abd5e9c5298e4a16d87d
NikitaLikinykh <likinykh.work@gmail.com>
2024-06-01 10:04:59 +0200

    fixNav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -71,6 +71,28 @@
         ></path>
       </svg>
     </label>
+    <label
+      title="favorite"
+      for="favorite"
+      class="nav__label"
+      :class="{ active: isActive('/favorite') }"
+      @click="clickNav('/favorite')"
+    >
+      <input id="favorite" name="page" type="radio" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="inherit"
+        viewBox="0 0 20 18"
+        height="18"
+        width="20"
+        class="icon favorite"
+      >
+        <path
+          fill="inherit"
+          d="M14.44 0C12.63 0 11.01 0.88 10 2.23C9.48413 1.53881 8.81426 0.977391 8.04353 0.590295C7.27281 0.203198 6.42247 0.00108555 5.56 0C2.49 0 0 2.5 0 5.59C0 6.78 0.19 7.88 0.52 8.9C2.1 13.9 6.97 16.89 9.38 17.71C9.72 17.83 10.28 17.83 10.62 17.71C13.03 16.89 17.9 13.9 19.48 8.9C19.81 7.88 20 6.78 20 5.59C20 2.5 17.51 0 14.44 0Z"
+        ></path>
+      </svg>
+    </label>
   </section>
 </template>
 
@@ -99,12 +121,4 @@ export default {
 };
 </script>
 
-<style scoped>
-.nav__label {
-  cursor: pointer;
-}
-
-.nav__label.active .icon {
-  fill: #d17842;
-}
-</style>
+<style scoped></style>
